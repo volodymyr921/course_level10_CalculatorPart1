eb56e06d838037e4e6b53077210ad378e1545a85
Volodymyr Omelianchuk <omelianchuk921@gmail.com>
2023-09-28 13:00:00 +0300

    Initial commit

---
+++ src/com/shpp/p2p/cs/vomelianchuk/assignment10/TestCalculator.java
@@ -0,0 +1,7 @@
+package com.shpp.p2p.cs.vomelianchuk.assignment10;
+
+public class TestCalculator {
+    public static void main(String[] args) {
+        Assignment10Part1.main(new String[]{"2 / a^2 + 2 *b^3 * 3", "a = 3", "b=2"});
+    }
+}
